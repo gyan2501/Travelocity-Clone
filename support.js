fcb65cb12538c34352e03d952ab7e51e341070bd
rakesh <dasrakesh81199@gmail.com>
2022-11-10 23:45:44 +0530

    added

---
+++ support.js
@@ -0,0 +1,23 @@
+ // const explore_more = () =>{
+    //     let rd_explore_div = document.getElementById("rd_explore_div");
+    //     rd_explore_div.style.display = "block";
+    // }
+
+    let rd_explore = document.getElementById("rd_explore");
+    rd_explore.addEventListener("click",()=>{
+        let rd_explore_div = document.getElementById("rd_explore_div");
+        rd_explore_div.style.display = "block";
+    });
+
+
+    let close_exp = document.getElementById("close_exp");
+    close_exp.addEventListener("click",()=>{
+        let rd_explore_div = document.getElementById("rd_explore_div");
+        rd_explore_div.style.display = "none";
+    });
+
+
+    let rd_last_explore = document.getElementById("rd_last_explore");
+    rd_last_explore.addEventListener("click",()=>{
+        window.location.href = "https://www.expediagroup.com/home/default.aspx";
+    })
