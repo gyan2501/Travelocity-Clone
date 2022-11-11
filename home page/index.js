9e8744c706f5d4f9a24b95bab1c73dc776a819e1
dibyaranjan1 <dibyaranjansahoo955@gmail.com>
2022-11-11 23:00:39 +0530

    add

---
+++ home page/index.js
@@ -0,0 +1,60 @@
+let uuuu = document.getElementById("uuuu")
+let ww = document.querySelector(".ww")
+
+uuuu.onclick=()=>{
+    ww.classList.toggle("yyy")
+}
+
+
+let z = document.getElementById("z")
+let aa = document.querySelector(".aa")
+
+z.onclick=()=>{
+    aa.classList.toggle("yyy")
+}
+
+
+
+let zz = document.getElementById("zz")
+let www = document.querySelector(".www")
+
+zz.onclick=()=>{
+    www.classList.toggle("yyy")
+}
+
+
+
+let zzz = document.getElementById("zzz")
+let wwww = document.querySelector(".wwww")
+
+zzz.onclick=()=>{
+    wwww.classList.toggle("yyy")
+}
+
+
+
+let zzzz = document.getElementById("zzzz")
+let wwwww = document.querySelector(".wwwww")
+
+zzzz.onclick=()=>{
+    wwwww.classList.toggle("yyy")
+}
+
+
+
+let zzzzz = document.getElementById("zzzzz")
+let c = document.querySelector(".c")
+
+zzzzz.onclick=()=>{
+    c.classList.toggle("yyy")
+}
+
+
+
+
+let ee = document.getElementById("sd")
+let we = document.querySelector(".we")
+
+ee.onclick=()=>{
+    we.classList.toggle("yyy")
+}
